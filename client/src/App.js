aa8ec9d03f15efdce223ec08b428911d4ad346d3
Yuriy Podnebesov <dartwader032@gmail.com>
2020-08-18 13:35:16 +0300

    added basic auth form

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Auth from './components/Auth';
 import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
+import {Button, 
+  Container, 
+  Card, 
+  Form,
+  Alert
+} from 'react-bootstrap';
 
 function App() {
+
+  const [auth, setAuth] = useState(false);
+
+  let email = auth ? 
+    <Form.Group>
+      <Form.Label>Enter your email:</Form.Label>
+      <Form.Control type = "email" />
+    </Form.Group>
+    : null;
+
+
   return (
     <div className="App">
-        <button className="btn btn-success" onClick = {clickHandler}>
-            Notification
-        </button>
+        <Auth />
     </div>
   );
 
---
+++ client/src/components/Auth.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+// import './Auth.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, 
+  Container, 
+  Card, 
+  Form,
+  Alert
+} from 'react-bootstrap';
+
+function Auth() {
+
+    const [auth, setAuth] = useState(false);
+  
+    let email = auth ? 
+      <Form.Group>
+        <Form.Label>Enter your email:</Form.Label>
+        <Form.Control type = "email" />
+      </Form.Group>
+      : null;
+  
+  
+    return (
+      <div className="Auth">
+          <Container style = {{minWidth: '350px', width: '40px'}}>
+            <Card>
+              <Card.Body>
+                <Alert style = {{fontSize: '1em', padding: '5px 10px' }} variant = "danger">Wrong data</Alert>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Enter your login:</Form.Label>
+                    <Form.Control type = "text" />
+                  </Form.Group>
+                  
+                  {email}
+  
+                  <Form.Group>
+                    <Form.Label>Enter your password:</Form.Label>
+                    <Form.Control type = "text" />
+                  </Form.Group>
+  
+  
+                  <Button 
+                    variant = 'primary' 
+                    size = 'lg'
+                    onClick = { () => setAuth(!auth) } 
+                    block>
+                    Submit
+                  </Button>
+  
+                </Form>    
+              </Card.Body>      
+            </Card>
+          </Container>
+      </div>
+    );
+
+    }
+
+  export default Auth;
